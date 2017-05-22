05a6a4dee82a40b65760f444300d1e70afea1099
Jan Rems <jan1.rems@gmail.com>
2017-05-22 12:05:02 +0200

    nadaljevanje

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -15,11 +15,44 @@ povprecja <- druzine %>% group_by(obcina) %>%
 
 # 3. faza: Vizualizacija podatkov
 
-#graf gibanja neenakosti v eu
+#iskanje ustreznih podatkov
+
+#skupni podatki za evropo
 
 evro <- inner_join(tabela_pred_transferji,tabela_prebivalci) %>%
   filter(Spol == "Skupaj"& Starost == "Skupaj") %>%
   group_by(Leto) %>%
   summarize(delez = sum(Delez * Populacija)/sum(Populacija))
 
+
+
+#podatki za leto 2007
+
+leto7_pred <- tabela_pred_transferji %>%
+  filter(Spol == "Skupaj"& Starost == "Skupaj",Leto == 2007) %>%
+  group_by(Drzava) %>%
+  select(Drzava, Delez)
+  
+povprecje_leto7_pred <- mean(leto7_pred$Delez)
+odklon_leto7_pred <- (1/length(leto7_pred)*sum((leto7_pred$Delez - povprecje_leto7_pred)^2))^0.5
+
+leto7_po <- tabela_po_transferjih %>%
+  filter(Spol == "Skupaj"& Starost == "Skupaj",Leto == 2007) %>%
+  group_by(Drzava) %>%
+  select(Drzava, Delez)
+
+povprecje_leto7_po <- mean(leto7_po$Delez)
+odklon_leto7_po <- (1/length(leto7_po)*sum((leto7_po$Delez - povprecje_leto7_po)^2))^0.5
+
+test <- right_join(tabela_pred_transferji, tabela_po_transferjih)
+
+najboljsi <- left_join(tabela_pred_transferji %>% rename(Delez.Pred = Delez),
+                       tabela_po_transferjih %>% rename(Delez.Po = Delez)) %>%
+  filter(Spol == "Skupaj"& Starost == "Skupaj",Leto == 2007) %>%
+  transmute(Drzava,  Delez = Delez.Pred - Delez.Po) %>%
+  arrange(desc(Delez))
+
+spol_pred  <- tabela_pred_transferji %>%
+  filter(Spol != "Skupaj" & Starost == "Skupaj" $ Leto == 2007) %>%
+  transmute(Drzava, Razlika = )
 
